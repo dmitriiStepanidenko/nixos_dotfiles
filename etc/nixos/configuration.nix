e968795dbd086a604cbc08e21fff167c5fd9f1c4
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-07-28 18:07:02 +0300

    73 current  2024-07-28 18:06:54  24.11.20240723.bfeb6e7   6.6.41                            *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -30,9 +30,8 @@
   # Because Nvivia?
   #boot.kernelParams = [ "module_blacklist=amdgpu" ];
 
-  #boot.initrd.kernelModules = ["nvidia"];
-  #boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
-  #boot.extraModulePackages = with config.boot.kernelPackages; [nvidia_x11];
+  boot.initrd.kernelModules = ["nvidia"];
+  boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
