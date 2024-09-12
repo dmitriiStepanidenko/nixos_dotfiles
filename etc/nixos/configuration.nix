bb2bc9d41f408defab91db4f99640556cc57b6d2
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-09-12 23:18:13 +0300

    110 current  2024-09-12 23:18:11  24.11.20240911.159be5d  6.6.50                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -112,13 +112,13 @@
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  #services.xserver.windowManager.leftwm.enable = true;
-  #services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.windowManager.leftwm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.defaultSession = "plasmax11";
+  #services.displayManager.sddm.enable = true;
+  #services.desktopManager.plasma6.enable = true;
+  #services.displayManager.defaultSession = "plasmax11";
 
   # Configure keymap in X11
   services.xserver.xkb = {
