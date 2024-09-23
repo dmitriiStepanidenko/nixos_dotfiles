5c4f2ed067f9639996369150064c5775fe2d0d0a
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-09-23 15:26:51 +0300

    176 current  2024-09-23 15:26:46  24.11.20240920.a1d9266  6.6.52                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -77,17 +77,17 @@
   #  };
   #};
   #services.tlp.enable = false;
-  #services.auto-cpufreq.enable = true;
-  #services.auto-cpufreq.settings = {
-  #    battery = {
-  #       governor = "powersave";
-  #       turbo = "never";
-  #    };
-  #    charger = {
-  #       governor = "performance";
-  #       turbo = "auto";
-  #    };
-  #};
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
 
   # Enable CROOON
   services.cron = {
