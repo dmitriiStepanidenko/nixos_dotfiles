4778ee9379979e418e9fb0b9a99fce53375e7c3d
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-09-25 14:40:14 +0300

    185 current  2024-09-25 14:40:08  24.11.20240920.a1d9266  6.6.52                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -205,7 +205,7 @@
 
   # Virtual Box
   virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [ "dmitrii" ];
+  users.extraGroups.vboxusers.members = ["dmitrii"];
   virtualisation.virtualbox.host.enableExtensionPack = true;
   #virtualisation.virtualbox.guest.enable = true;
   #virtualisation.virtualbox.guest.draganddrop = true;
@@ -213,7 +213,6 @@
   # Libvirt
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
-
 
   # Install firefox.
   programs.firefox.enable = true;
