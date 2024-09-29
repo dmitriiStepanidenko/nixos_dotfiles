29a88e2fe7ec27b9ca495c70dffe91818eedc95e
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-09-29 13:18:00 +0300

    189 current  2024-09-29 13:17:54  24.11.20240928.b5b2fec  6.6.52                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -267,6 +267,7 @@
   # Default browser
   xdg.mime.defaultApplications = {
     "text/html" = "firefox.desktop";
+    "application/pdf" = "firefox.desktop";
     "x-scheme-handler/http" = "firefox.desktop";
     "x-scheme-handler/https" = "firefox.desktop";
     "x-scheme-handler/about" = "firefox.desktop";
