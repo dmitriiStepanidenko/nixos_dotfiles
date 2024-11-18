5a61ee977f211e616b9db634524e36cd8497023b
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-11-18 19:53:56 +0300

    258 current  2024-11-18 19:53:49  25.05.20241118.5083ec8  6.6.61                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -24,7 +24,7 @@ in {
     #./home/dmitrii/shared/dotfiles/etc/nixos/modules/wireguard.nix
     #./modules/wireguard.nix
   ];
-  boot.kernelModules = ["coretemp"];
+  boot.kernelModules = ["coretemp" "ideapad-laptop"];
 
   # Cuz Nvidia. I need 535 version
   #boot.kernelPackages = inputs.nixpkgsunstable.linuxPackages_latest;
