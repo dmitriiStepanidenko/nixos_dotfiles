914c35d5feba52e0e0f4f3c02e68b1154e19234c
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2024-11-27 17:50:49 +0300

    fiv

--- config/nvim/after/plugin/codesnap.lua
+++ config/nvim/after/plugin/codesnap.lua
@@ -7,10 +7,15 @@ require("codesnap").setup({
   -- save_path = "~/Pictures/foo.png"
   -- parsed: "~/Pictures/foo.png"
   save_path = "~",
+  mac_window_bar = false,
+  has_breadcrumbs = false, -- shows file 
+  show_workspace = false,
   watermark = "",
   bg_theme = "sea",
-  bg_x_padding = 58,
-  bg_y_padding = 39,
+  --bg_x_padding = 29,
+  --bg_y_padding = 19,
+  bg_x_padding = 3,
+  bg_y_padding = 2,
   bg_padding = nil,
   has_line_number = true,
   has_breadcrumbs = true,
