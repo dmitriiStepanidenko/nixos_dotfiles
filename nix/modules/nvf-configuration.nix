eb14fe0b3ab02330e0b1d6e86d2fe3d13d160338
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-02-17 14:46:47 +0300

    435 current  2025-02-17 14:46:39  24.11.20250213.0b73e36  6.6.76                           *

--- nix/modules/nvf-configuration.nix
+++ nix/modules/nvf-configuration.nix
@@ -105,15 +105,15 @@
       motion.leap = {
         enable = true;
         mappings = {
-          leapForwardTo = "\\\\s";
-          leapBackwardTo = "\\\\S";
+          leapForwardTo = "\\s";
+          leapBackwardTo = "\\S";
         };
       };
       vim-wakatime.enable = true;
       outline = {
         aerial-nvim = {
           enable = true;
-          mappings.toggle = "\\\\l";
+          mappings.toggle = "\\l";
         };
       };
     };
