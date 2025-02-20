fa5328132d6022c4ac13930a62391f6c061f6c38
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-02-20 23:20:16 +0300

    453 current  2025-02-20 23:20:04  24.11.20250217.c618e28  6.6.77                           *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -343,6 +343,8 @@ in {
     };
   };
 
+  environment.memoryAllocator.provider = "jemalloc";
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
