fa3cd5490bf611b61477a7287dba8d68ff2a23a6
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-05-21 00:32:45 +0300

    582 current  2025-05-21 00:32:43  24.11.20250517.9b5ac7a  6.6.90                          *

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -449,6 +449,42 @@ in {
     terminal = "screen-256color";
     enable = true;
     keyMode = "vi";
+    clock24 = true;
+    extraConfigBeforePlugins = ''
+      set -g status-left-length 80
+
+      # Tokyo night
+      set -g @tokyo-night-tmux_theme storm    # storm | day | default to 'night'
+      set -g @tokyo-night-tmux_transparent 1  # 1 or 0
+
+      set -g @tokyo-night-tmux_window_id_style digital
+      set -g @tokyo-night-tmux_pane_id_style hsquare
+      set -g @tokyo-night-tmux_zoom_id_style dsquare
+
+      # Icon styles
+      set -g @tokyo-night-tmux_terminal_icon 
+      set -g @tokyo-night-tmux_active_terminal_icon 
+
+      # No extra spaces between icons
+      set -g @tokyo-night-tmux_window_tidy_icons 0
+
+      set -g @tokyo-night-tmux_show_datetime 0
+      set -g @tokyo-night-tmux_date_format DMY
+      set -g @tokyo-night-tmux_time_format 24H
+
+      set -g @tokyo-night-tmux_show_path 1
+      set -g @tokyo-night-tmux_path_format relative # 'relative' or 'full'
+
+      set -g @tokyo-night-tmux_show_netspeed 1
+      set -g @tokyo-night-tmux_netspeed_iface "wlan0" # Detected via default route
+      set -g @tokyo-night-tmux_netspeed_showip 1      # Display IPv4 address (default 0)
+      set -g @tokyo-night-tmux_netspeed_refresh 1     # Update interval in seconds (default 1)
+
+      set -g @tokyo-night-tmux_show_battery_widget 1
+      set -g @tokyo-night-tmux_battery_name "BAT1"  # some linux distro have 'BAT0'
+      set -g @tokyo-night-tmux_battery_low_threshold 21 # default
+
+    '';
     extraConfig = ''
       bind h select-pane -L
       bind j select-pane -D
@@ -480,7 +516,7 @@ in {
     '';
     plugins = with pkgs; [
       tmuxPlugins.tokyo-night-tmux
-      tmuxPlugins.tmux-powerline
+      #tmuxPlugins.tmux-powerline
     ];
   };
 
