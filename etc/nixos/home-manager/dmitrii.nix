682da837c826273a5e124c1c4b00bf9401e16353
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-05-25 01:14:31 +0300

    610 current  2025-05-25 01:14:27  25.05.20250524.9c585ee  6.13.12                          *

--- etc/nixos/home-manager/dmitrii.nix
+++ etc/nixos/home-manager/dmitrii.nix
@@ -9,10 +9,33 @@
 
     stateVersion = "25.05";
   };
+  programs = {
+    git = {
+      enable = true;
+      delta.enable = true;
+    };
+    home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    delta.enable = true;
+    alacritty = {
+      enable = true;
+      settings = {
+        window.opacity = 0.97;
+        font = {
+          normal = {
+            family = "Fira Code";
+            style = "Regular";
+          };
+          bold = {
+            family = "Fira Code";
+            style = "Bold";
+          };
+          italic = {
+            family = "Fira Code";
+            style = "Italic";
+          };
+        };
+      };
+      theme = "tokyo_night_enhanced";
+    };
   };
-  programs.home-manager.enable = true;
 }
