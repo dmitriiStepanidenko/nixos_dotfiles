bbcb190901b5140ee2be472cbb545496ddaa546d
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-05-24 16:33:40 +0300

    598 current  2025-05-24 16:33:37  25.05.20250524.c7b0e02  6.12.30                          *

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -16,15 +16,9 @@
     nixpkgs.follows = "nixos-25-05";
     nixpkgs_unstable.follows = "nixos-unstable";
 
-    nixos-24-11-stable-xsecurelock.url = "github:nixos/nixpkgs?ref=d3c42f187194c26d9f0309a8ecc469d6c878ce33";
+    #nixos-24-11-stable-xsecurelock.url = "github:nixos/nixpkgs?ref=d3c42f187194c26d9f0309a8ecc469d6c878ce33";
 
-    neovim-nightly-overlay.url = "https://github.com/nix-community/neovim-nightly-overlay/archive/1f54e89757bd951470a9dcc8d83474e363f130c5.tar.gz";
-    nixvim = {
-      #url = "github:nix-community/nixvim";
-      url = "github:nix-community/nixvim?ref=3d24cb72618738130e6af9c644c81fe42aa34ebc";
-
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    #neovim-nightly-overlay.url = "https://github.com/nix-community/neovim-nightly-overlay/archive/1f54e89757bd951470a9dcc8d83474e363f130c5.tar.gz";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
