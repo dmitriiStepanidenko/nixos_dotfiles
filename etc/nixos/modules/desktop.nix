c17ee7b371a082e83b30da093c1fd319bba1962a
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-05-25 20:59:20 +0300

    627 current  2025-05-25 20:59:15  25.05.20250524.9c585ee  6.13.12                          *

--- etc/nixos/modules/desktop.nix
+++ etc/nixos/modules/desktop.nix
@@ -71,8 +71,9 @@ in {
     portal = {
       enable = true;
       extraPortals = [
-        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-gnome
       ];
+      config.common.default = ["gnome"];
       xdgOpenUsePortal = true;
     };
   };
