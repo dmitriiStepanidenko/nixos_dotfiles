94022f9d989d895738c059db29597955eb963067
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-05-28 04:20:11 +0300

    653 current  2025-05-28 04:20:05  25.05.20250524.9c585ee  6.13.12                          *

--- nix/packages/cosmic-ext-applet-privacy-indicator.nix
+++ nix/packages/cosmic-ext-applet-privacy-indicator.nix
@@ -8,53 +8,66 @@
   libxkbcommon,
   pipewire,
   wayland,
+  libGL,
   pkg-config,
   ...
-}:
-stdenvNoCC.mkDerivation rec {
-  pname = "cosmic-ext-applet-privacy-indicator";
-  version = "0.1.2";
-
-  src = fetchzip {
-    url = "https://github.com/D-Brox/cosmic-ext-applet-privacy-indicator/releases/download/v${version}/cosmic-ext-applet-privacy-indicator-x86_64.tar.gz";
-    hash = "sha256-sD7lhUMeQylZ78sa9DHL1KafDVziVJxiGn2N7K42Fuk=";
-    inherit pname version;
-  };
-  #dontBuild = true;
-  #dontConfigure = true;
-
-  nativeBuildInputs = [
-    glibc
-    autoPatchelfHook
+}: let
+  libPath = lib.makeLibraryPath [
+    libGL
     libxkbcommon
-    pipewire
     wayland
-    #pkg-config
-    #rustPlatform.bindgenHook
-    #wayland
   ];
-  buildInputs = [
-    gcc-unwrapped
-    wayland
-    pipewire
-  ];
+in
+  stdenvNoCC.mkDerivation rec {
+    pname = "cosmic-ext-applet-privacy-indicator";
+    version = "0.1.2";
 
-  installPhase = ''
-    runHook preInstall
-    ls
-    pwd
-    mkdir -p $out/bin
-    mkdir -p $out/share/applications
-    mkdir -p $out/share/metainfo
+    src = fetchzip {
+      url = "https://github.com/D-Brox/cosmic-ext-applet-privacy-indicator/releases/download/v${version}/cosmic-ext-applet-privacy-indicator-x86_64.tar.gz";
+      hash = "sha256-sD7lhUMeQylZ78sa9DHL1KafDVziVJxiGn2N7K42Fuk=";
+      inherit pname version;
+    };
+    #dontBuild = true;
+    #dontConfigure = true;
 
-    install -m755 cosmic-ext-applet-privacy-indicator/cosmic-ext-applet-privacy-indicator $out/bin
-    cp cosmic-ext-applet-privacy-indicator/dev.DBrox.CosmicPrivacyIndicator.desktop $out/share/applications/CosmicPrivacyIndicator.desktop
-    cp cosmic-ext-applet-privacy-indicator/dev.DBrox.CosmicPrivacyIndicator.metainfo.xml $out/share/metainfo/CosmicPrivacyIndicator.metainfo.xml
-    runHook postInstall
-  '';
-  meta = {
-    description = " Privacy indicator for the COSMIC DE";
-    homepage = "https://github.com/D-Brox/cosmic-ext-applet-privacy-indicator";
-    license = lib.licenses.gpl3;
-  };
-}
+    nativeBuildInputs = [
+      glibc
+      autoPatchelfHook
+      libxkbcommon
+      pipewire
+      wayland
+      #pkg-config
+      #rustPlatform.bindgenHook
+      #wayland
+    ];
+    buildInputs = [
+      gcc-unwrapped
+      wayland
+      pipewire
+      libGL
+      libxkbcommon
+      pkg-config
+    ];
+
+    installPhase = ''
+      runHook preInstall
+      ls
+      pwd
+      mkdir -p $out/bin
+      mkdir -p $out/share/applications
+      mkdir -p $out/share/metainfo
+
+      install -m755 cosmic-ext-applet-privacy-indicator/cosmic-ext-applet-privacy-indicator $out/bin
+      cp cosmic-ext-applet-privacy-indicator/dev.DBrox.CosmicPrivacyIndicator.desktop $out/share/applications/CosmicPrivacyIndicator.desktop
+      cp cosmic-ext-applet-privacy-indicator/dev.DBrox.CosmicPrivacyIndicator.metainfo.xml $out/share/applications/CosmicPrivacyIndicator.metainfo.xml
+      runHook postInstall
+    '';
+    env = {
+      LD_LIBRARY_PATH = libPath;
+    };
+    meta = {
+      description = " Privacy indicator for the COSMIC DE";
+      homepage = "https://github.com/D-Brox/cosmic-ext-applet-privacy-indicator";
+      license = lib.licenses.gpl3;
+    };
+  }
