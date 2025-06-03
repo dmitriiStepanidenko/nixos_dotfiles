82d290e5e9db9efbf6abfee060104c3c3e3cfc40
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-06-04 02:01:43 +0300

    717 current  2025-06-04 02:01:36  25.05.20250602.2d2a4c7  6.13.12                          *

--- etc/nixos/home-manager/hyprland.nix
+++ etc/nixos/home-manager/hyprland.nix
@@ -125,21 +125,13 @@ in {
         #      "eDP-1"
         #      "HDMI-A-1"
         #    ];
-        modules-left = ["hyprland/workspaces" "hyprland/mode" "hyprland/taskbar"];
-        modules-center = ["hyprland/window" "custom/hello-from-waybar"];
-        modules-right = ["mpd" "custom/mymodule#with-css-id" "temperature"];
+        modules-left = ["hyprland/workspaces" "wlr/taskbar" "hyprland/window"];
+        modules-center = ["clock" "battery" "temperature"];
+        modules-right = ["mpd" "wireplumber" "backlight" "load" "memory"];
 
-        "sway/workspaces" = {
+        "hyprland/workspaces" = {
           disable-scroll = true;
           all-outputs = true;
-        };
-        "custom/hello-from-waybar" = {
-          format = "hello {}";
-          max-length = 40;
-          interval = "once";
-          exec = pkgs.writeShellScript "hello-from-waybar" ''
-            echo "from within waybar"
-          '';
         };
       };
     };
