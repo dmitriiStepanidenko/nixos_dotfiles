79cdc420f4bc00c98d06c4afe1f92f2362ce9c52
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-06-04 01:57:16 +0300

    716 current  2025-06-04 01:57:10  25.05.20250602.2d2a4c7  6.13.12                          *

--- etc/nixos/home-manager/hyprland.nix
+++ etc/nixos/home-manager/hyprland.nix
@@ -20,11 +20,11 @@ in {
     portalPackage = null;
     settings = {
       #exec-once = ''${startupScript}/bin/start'';
-      general = {
-        lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
-        before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
-        after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
-      };
+      #general = {
+      #lock_cmd = "pidof hyprlock || hyprlock"; # avoid starting multiple hyprlock instances.
+      #before_sleep_cmd = "loginctl lock-session"; # lock before suspend.
+      #after_sleep_cmd = "hyprctl dispatch dpms on"; # to avoid having to press a key twice to turn on the display.
+      #};
       exec-once = [
         "${pkgs.waybar}/bin/waybar 2>&1 > ~/waybar.log"
         #"${pkgs.swww}/bin/swww init 2>&1 > ~/swww_init.log &"
