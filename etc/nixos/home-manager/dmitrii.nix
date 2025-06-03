75dfb0ea40af25f75b91cf4f08839df1e5edcfe1
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-06-03 21:26:32 +0300

    697 current  2025-06-03 21:26:26  25.05.20250602.2d2a4c7  6.13.12                          *

--- etc/nixos/home-manager/dmitrii.nix
+++ etc/nixos/home-manager/dmitrii.nix
@@ -106,25 +106,24 @@ in {
       exec-once = ''${startupScript}/bin/start'';
       "$terminal" = "alacritty";
       "$mod" = "SUPER";
-      bind =
-        [
-          "$mod, F, exec, firefox"
-          #", Print, exec, grimblast copy area"
-          "$mod, P, exec, ${pkgs.wofi}/bin/wofi"
-        ]
-        ++ (
-          # workspaces
-          # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-          builtins.concatLists (builtins.genList (
-              i: let
-                ws = i + 1;
-              in [
-                "$mod, code:1${toString i}, workspace, ${toString ws}"
-                "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-              ]
-            )
-            9)
-        );
+      bind = [
+        "$mod, F, exec, firefox"
+        "$mod, P, exec, ${pkgs.wofi}/bin/wofi"
+        #", Print, exec, grimblast copy area"
+      ];
+      #++ (
+      #  # workspaces
+      #  # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
+      #  builtins.concatLists (builtins.genList (
+      #      i: let
+      #        ws = i + 1;
+      #      in [
+      #        "$mod, code:1${toString i}, workspace, ${toString ws}"
+      #        "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+      #      ]
+      #    )
+      #    9)
+      #);
     };
   };
   home.pointerCursor = {
