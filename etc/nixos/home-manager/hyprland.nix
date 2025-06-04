ee959d52206d0ec1496c7172fd8f63c0a5e07820
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-06-04 17:05:51 +0300

    741 current  2025-06-04 17:05:44  25.05.20250602.2d2a4c7  6.13.12                          *

--- etc/nixos/home-manager/hyprland.nix
+++ etc/nixos/home-manager/hyprland.nix
@@ -91,10 +91,8 @@ in {
     enable = true;
     settings = {
       general = {
-        disable_loading_bar = true;
         grace = 300;
         hide_cursor = true;
-        no_fade_in = false;
       };
       background = [
         {
