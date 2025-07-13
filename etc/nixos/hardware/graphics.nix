f79ea5a3508ffd950861400ca6f6c5c08c73326d
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-07-13 13:55:58 +0300

    838 current  2025-07-13 13:55:54  25.05.20250702.f81bb6b  6.13.12                          *

--- etc/nixos/hardware/graphics.nix
+++ etc/nixos/hardware/graphics.nix
@@ -63,7 +63,15 @@ in {
     # accessible via `nviia-settings`.
     nvidiaSettings = true;
 
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    #package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "575.64.03";
+      sha256_64bit = "sha256-S7eqhgBLLtKZx9QwoGIsXJAyfOOspPbppTHUxB06DKA=";
+      sha256_aarch64 = "sha256-s2Jm2wjdmXZ2hPewHhi6hmd+V1YQ+xmVxRWBt68mLUQ=";
+      openSha256 = "sha256-SAl1+XH4ghz8iix95hcuJ/EVqt6ylyzFAao0mLeMmMI=";
+      settingsSha256 = "sha256-o8rPAi/tohvHXcBV+ZwiApEQoq+ZLhCMyHzMxIADauI=";
+      persistencedSha256 = "sha256-/3OAZx8iMxQLp1KD5evGXvp0nBvWriYapMwlMSc57h8=";
+    };
     #package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
     #  version = "555.58.02";
     #  sha256_64bit = "sha256-xctt4TPRlOJ6r5S54h5W6PT6/3Zy2R4ASNFPu8TSHKM=";
