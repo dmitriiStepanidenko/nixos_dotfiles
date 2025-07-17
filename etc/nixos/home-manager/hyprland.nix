5d04b5b13eaa0c2758f03eb5465e1ef89bf0e07d
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-07-17 13:52:45 +0300

    842 current  2025-07-17 13:52:42  25.05.20250713.483dd88  6.15.5                           *

--- etc/nixos/home-manager/hyprland.nix
+++ etc/nixos/home-manager/hyprland.nix
@@ -42,8 +42,8 @@
 
     if [ "$nvidia_present" = true ]; then
       echo "NVIDIA GPU detected/enabled, using NOTHING instead of hibernation"
-      #systemctl suspend
-      systemctl hibernate
+      systemctl suspend
+      #systemctl hibernate
     else
       echo "No NVIDIA GPU detected/enabled, safe to hibernate"
       systemctl hibernate
