fcb8b3a0b58853f5f2a62ff6e2052d5583759f47
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-10-11 16:17:30 +0300

    976 current  2025-10-11 16:17:25  25.05.20250929.5ed4e25  6.16.9                          *

--- nix/modules/nvf-configuration.nix
+++ nix/modules/nvf-configuration.nix
@@ -72,6 +72,13 @@
           enable_modified_markers = true;
           enable_opened_markers = true;
           filesystem.hijack_netrw_behavior = "open_current";
+          buffers = {
+            follow_current_file = {
+              enabled = true; #-- This will find and focus the file in the active buffer every time
+              #--              -- the current file is changed while the tree is open.
+              leave_dirs_open = false; #-- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            };
+          };
         };
       };
       # nvimTree = {
